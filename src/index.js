d5d71ae63ac0e7a609938b5980407c4b506f538e
rdbmw <rdbmw@mail.ru>
2016-11-07 13:03:09 +0300

    Task 2A: A+B

--- src/index.js
+++ src/index.js
@@ -1,4 +1,88 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
+import express from 'express';
+import fetch from 'isomorphic-fetch';
+import Promise from 'bluebird';
+import _ from 'lodash';
+import canonize from './canonize';
+
+const __DEV__ = true;
+
+const app = express();
+app.get('/canonize', (req, res) => {
+  const userName = canonize(req.query.url);
+  res.json({
+    url: req.query.url,
+    userName });
+});
+
+const baseUrl = 'https://pokeapi.co/api/v2';
+const pokemonFields = ['id', 'name', 'base_experience', 'height', 'is_default', 'order', 'weight'];
+
+async function getPokemons(url, i = 0) {
+  // console.log('getPokemons ', url, i);
+  const response = await fetch(url);
+  const page = await response.json();
+  const pokemons = page.results;
+  if (__DEV__ && i > 1) {
+    return pokemons;
+  }
+  if (page.next) {
+    const pokemons2 = await getPokemons(page.next, i + 1);
+    return [
+      ...pokemons,
+      ...pokemons2];
+  }
+  return pokemons;
 }
-hello('JS World');
+
+async function getPokemon(url) {
+  // console.log('getPokemon ', url);
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  return pokemon;
+}
+// const pokemonsUrl = `${baseUrl}/pokemon`;
+// getPokemons(pokemonsUrl).then((pokemons) => {
+//   console.log(pokemons.length);
+// });
+
+
+app.get('/', async (req, res) => {
+  try {
+    const pokemonsUrl = `${baseUrl}/pokemon`;
+    const pokemonsInfo = await getPokemons(pokemonsUrl);
+    // const pokemonsPromises = pokemonsInfo.slice(0, 2).map(info => getPokemon(info.url));
+    const pokemonsPromises = pokemonsInfo.map(info => getPokemon(info.url));
+    const pokemonsFull = await Promise.all(pokemonsPromises);
+    const pokemons = pokemonsFull.map(pokemon => _.pick(pokemon, pokemonFields));
+    const sortPokemons = _.sortBy(pokemons, pokemon => -pokemon.weight);
+    return res.json(sortPokemons);
+  } catch (err) {
+    console.log(err);
+    return res.json(err);
+  }
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/task2A', (req, res) => {
+  let a = +req.query.a;
+  let b = +req.query.b;
+  if (isNaN(a)) {
+    a = 0;
+  }
+  if (isNaN(b)) {
+    b = 0;
+  }
+  // console.log('a=', a);
+  // console.log('b=', b);
+  // console.log('a+b=', a + b);
+  res.json(a + b);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
