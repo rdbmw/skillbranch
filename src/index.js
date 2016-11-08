73980cdf5e4be303b571a72504f785218f3912c3
rdbmw <rdbmw@mail.ru>
2016-11-08 14:21:16 +0300

    Task 2B

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import fetch from 'isomorphic-fetch';
 import Promise from 'bluebird';
 import _ from 'lodash';
+
 import canonize from './canonize';
 
 const __DEV__ = true;
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Lesson 1. Hello JS World. Function Canonize
 app.get('/canonize', (req, res) => {
   const userName = canonize(req.query.url);
   res.json({
@@ -14,6 +22,7 @@ app.get('/canonize', (req, res) => {
     userName });
 });
 
+// Lesson 1. Hello JS World. PokeApi
 const baseUrl = 'https://pokeapi.co/api/v2';
 const pokemonFields = ['id', 'name', 'base_experience', 'height', 'is_default', 'order', 'weight'];
 
@@ -40,34 +49,28 @@ async function getPokemon(url) {
   const pokemon = await response.json();
   return pokemon;
 }
+
 // const pokemonsUrl = `${baseUrl}/pokemon`;
 // getPokemons(pokemonsUrl).then((pokemons) => {
 //   console.log(pokemons.length);
 // });
 
-
 app.get('/', async (req, res) => {
   try {
     const pokemonsUrl = `${baseUrl}/pokemon`;
     const pokemonsInfo = await getPokemons(pokemonsUrl);
-    // const pokemonsPromises = pokemonsInfo.slice(0, 2).map(info => getPokemon(info.url));
     const pokemonsPromises = pokemonsInfo.map(info => getPokemon(info.url));
     const pokemonsFull = await Promise.all(pokemonsPromises);
     const pokemons = pokemonsFull.map(pokemon => _.pick(pokemon, pokemonFields));
     const sortPokemons = _.sortBy(pokemons, pokemon => -pokemon.weight);
     return res.json(sortPokemons);
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     return res.json(err);
   }
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
+// Lesson 1. Hello JS World. Task 2A: A+B
 app.get('/task2A', (req, res) => {
   let a = +req.query.a;
   let b = +req.query.b;
@@ -81,6 +84,31 @@ app.get('/task2A', (req, res) => {
   // console.log('b=', b);
   // console.log('a+b=', a + b);
   res.json(a + b);
+});
+
+// Lesson 1. Hello JS World. Task 2B: Surname Name Patronym -> Surname N. P.
+app.get('/task2B', (req, res) => {
+  const fullName = String.trim(req.query.fullname).replace(/\s+/g, ' ');
+  let editName;
+  if (fullName.search(/[0-9_/]/) === -1) {
+    const strArr = fullName.split(' ');
+    switch (strArr.length) {
+      case 3:
+        editName = `${strArr[2].charAt(0).toUpperCase()}${strArr[2].slice(1).toLowerCase()} ${strArr[0].charAt(0).toUpperCase()}. ${strArr[1].charAt(0).toUpperCase()}.`;
+        break;
+      case 2:
+        editName = `${strArr[1].charAt(0).toUpperCase()}${strArr[1].slice(1).toLowerCase()} ${strArr[0].charAt(0).toUpperCase()}.`;
+        break;
+      case 1:
+        editName = strArr[0] === '' ? 'Invalid fullname' : `${strArr[0].charAt(0).toUpperCase()}${strArr[0].slice(1).toLowerCase()}`;
+        break;
+      default:
+        editName = 'Invalid fullname';
+    }
+  } else {
+    editName = 'Invalid fullname';
+  }
+  res.send(editName);
 });
 
 app.listen(3000, () => {
