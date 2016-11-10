33a74ad7cffffaf3bc807228be8fb80eb710d1f4
rdbmw <rdbmw@mail.ru>
2016-11-10 14:44:12 +0300

    lesson #3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import Promise from 'bluebird';
+import bodyParser from 'body-parser';
 import _ from 'lodash';
+import mongoose from 'mongoose';
 
 import canonize from './lesson1/canonize';
 import { getPokemons, getPokemon } from './lesson1/pokeapi';
+import saveDataInDb from './lesson3/saveDataInDb';
+import Pet from './lesson3/models/Pet';
+import User from './lesson3/models/User';
+import isAdmin from './lesson3/middlewares/isAdmin';
 
 const app = express();
 app.use((req, res, next) => {
@@ -11,6 +17,61 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+app.use(bodyParser.json());
+//app.use(isAdmin);
+
+// ---------------------------------------------------------------------------//
+  // Lesson 3. Express.js & MongoDB.
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://publicdb.mgbeta.ru/rdbmw_skb3');
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  return res.json(users);
+});
+
+app.get('/pets', async (req, res) => {
+  const pets = await Pet.find().populate('owner');
+  return res.json(pets);
+});
+
+app.post('/data', async (req, res) => {
+  const data = req.body;
+  if (!data.user) return res.status(400).send('user required');
+  if (!data.pets) data.pets = [];
+  const user = await User.findOne({
+    name: data.user.name,
+  });
+  if (user) return res.status(400).send('user.name is exists');
+  try {
+    const result = await saveDataInDb(data);
+    return res.json(result);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.get('/clear', isAdmin, async (req, res) => {
+  await User.remove({});
+  await Pet.remove({});
+  return res.send('ok');
+});
+
+// {
+//  "user": {
+//    "name": "rdbmw"
+//  },
+//  "pets": [
+//    {
+//      "name": "Pet1",
+//      "type": "cat"
+//    },
+//    {
+//      "name": "Pet2",
+//      "type": "dog"
+//    }
+//  ]
+// }
 
 // ---------------------------------------------------------------------------//
 // Lesson 2. Hello JS World. Task 2B: Surname Name Patronym -> Surname N. P.
